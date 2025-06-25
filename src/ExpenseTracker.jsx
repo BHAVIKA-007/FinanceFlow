5cc246bf23ad5f8f0071ef88a3c177adcda41520
BHAVIKA-007 <bhavikaboddireddy03@gmail.com>
2025-06-25 17:38:19 +0530

    changes to expensetracker.jsx

--- src/ExpenseTracker.jsx
+++ src/ExpenseTracker.jsx
@@ -1,5 +1,5 @@
 import './ExpenseTracker.css';
-import React, { useState, useEffect } from 'react';
+import React, {useState} from 'react';
 
 function ExpenseTracker(props){
 const [expense, setExpense] = useState({
